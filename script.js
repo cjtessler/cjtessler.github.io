bd55281947e75a390f55eaa60dbe4040a31df027
Cody Tessler <codytessler@gmail.com>
2023-02-28 23:24:43 -0500

    Using github API

--- script.js
+++ script.js
@@ -12,5 +12,27 @@ request.open("GET", "https://api.github.com/users/cjtessler/repos", true);
 request.send();
 
 function loadData(data) {
-  console.log(data);
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+  // Reference project container
+  const projectContainer = document.getElementById("project-container");
+  let repo = data["full_name"];
+  let date = new Date(data["updated_at"]).toLocaleDateString("en-us", options);
+
+  // Create repo card
+  const project = document.createElement("div");
+  project.classList = "repo-card";
+  project.setAttribute("data-repo", repo);
+  projectContainer.appendChild(project);
+
+  // Create subtext for last commit
+  const subtext = document.createElement("div");
+  subtext.classList = "subtext";
+  subtext.innerHTML = `Last commit on ${date}.`;
+  projectContainer.appendChild(subtext);
 }
